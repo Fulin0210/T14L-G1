7bce3e716f08b2e0e243189e14b5949461464f99
LauJunXing <124169058+LauJunXing@users.noreply.github.com>
2023-02-01 23:38:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
 // Lab: T14L 
-// Names: KAWSAR | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: 1211310827 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: mdrisveyhasan1@gmail.com | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: +601169727851 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Names: KAWSAR | CHEN FU LIN | LAU JUN XING 
+// IDs: 1211310827 | 1221301545 | 1201102941
+// Emails: mdrisveyhasan1@gmail.com | fulincheng1018@gmail.com | xinglau92@gmail.com
+// Phones: +601169727851 | +60163864848 | +60164074733
 // ********************************************************* 
 
 
